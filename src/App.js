49d7cb039c742114e2003fcfb794816ec327d872
gsim2345 <gb@gabriellasimon.me>
2019-08-22 21:14:32 +0200

    Added containers and Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Route, Redirect, Switch} from 'react-router-dom';
 import Appbar from "./components/Appbar";
+import CountryList from "./containers/CountryList";
+import Country from "./containers/Country";
+
 
 function App() {
   return (
     <div >
       <Appbar />
+      <Switch>
+          <Route path="/countrylist" component={CountryList} />
+          <Route path="/:country" component={Country} />
+          <Route path="/" exact component={CountryList} />
+          <Redirect to='/' />
+        </Switch>
     </div>
   );
 }
